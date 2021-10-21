f84e4f317acd226b00e09eccabfded63f3e73354
Muhammed Lateef <92058597+Lateef95@users.noreply.github.com>
2021-10-21 22:46:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,76 @@
+#x = 15 
+#y = 7 
+
+#if x > y:
+  #print ('yes')
+
+#=========================
+
+#x = 5 
+#y = 7 
+
+#if x < y:
+  #print ('no')
+
+#if 'hello' in 'hello world':
+  #print ('yes')
+
+#================================
+
+#x = 8 
+#y = 7 
+
+#if x > y:
+  #print ('yes it is greater')
+
+#elif x > y:
+  #print ('no it is not')
+
+#else:
+  #print ('neither')
+
+#=========================
+
+#nested if statement 
+
+#num = 0
+
+#if num >=0:
+  #if num ==0:
+    #print ('zero')
+  
+  #else:
+    #print ('postive number')
+
+#else:
+  #print ('negative number')
+
+#=================================
+
+#one line if statement 
+
+#if 7 > 5: print('yes it is')
+
+#================================
+
+#Tenary operator (no colons used) 
+
+print('yes') if 7 > 5 else print('no')
+
+if 7 > 5: 
+  print ('yes')
+
+else:
+   print ('no')
+
+
+#======================================
+
+
+
+
+
+
+
+
+
